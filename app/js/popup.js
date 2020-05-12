65c6cfe3e829da17947f5d70fa68476d024d8f73
marwanmar123 <youcode098@gmail.com>
2020-05-12 08:39:07 +0000

    respopop

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,5 +1,10 @@
 popup.addEventListener('click', function () {
     document.querySelector('#popup').style.display = "block";
+    window.addEventListener('scroll', noscroll);
+
+    function noscroll() {
+        window.scrollTo(0, 0);
+    }
 });
 document.querySelector('.popimg').addEventListener('click', function () {
     document.querySelector('#popup').style.display = "none";
--- src/js/general.js
+++ src/js/general.js
@@ -18,6 +18,11 @@ document.querySelector('.close').addEventListener('click', function () {
 })
 popup.addEventListener('click', function () {
     document.querySelector('#popup').style.display = "block";
+    window.addEventListener('scroll', noscroll);
+
+    function noscroll() {
+        window.scrollTo(0, 0);
+    }
 });
 document.querySelector('.popimg').addEventListener('click', function () {
     document.querySelector('#popup').style.display = "none";
