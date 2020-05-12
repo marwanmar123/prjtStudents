52cb072ee6b36df4fa2e932f21c0fba10681666d
marwanmar123 <youcode098@gmail.com>
2020-05-12 09:47:41 +0000

    noscroll

--- src/js/general.js
+++ src/js/general.js
@@ -12,12 +12,9 @@ signInButton.addEventListener('click', () => {
 });
 popup.addEventListener('click', function () {
     document.querySelector('#popup').style.display = "block";
-    window.addEventListener('scroll', noscroll);
-
-    function noscroll() {
-        window.scrollTo(0, 0);
-    }
+    document.body.style.overflow = "hidden";
 });
-document.querySelector('#close').addEventListener('click', function () {
+document.querySelector('.close').addEventListener('click', function () {
     document.querySelector('#popup').style.display = "none";
+    document.body.style.overflow = "auto";
 });
